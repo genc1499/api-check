9035c15695a71e9f57330409fe0bb43aaa137352
Joe Gencarelli <jp.genc@gmail.com>
2022-07-14 01:49:49 -0400

    Last test with react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
 import axios from 'axios';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+  const [country,setCountry] = useState("")
+  const [city,setCity] = useState("")
+
+const search = () =>{
   axios({
 
     url:'https://api.geoapify.com/v1/geocode/search?REQUEST_PARAMS',
  
     params: {
       apiKey: '54126e5ad21e4bcd8fe3defb8fbbb57f',
-      city: 'Toronto',
+      city: city,
+      country:country,
     }
   }).then((res)=>{
-    console.log(res.data.features[0].properties.lon);
-    console.log(res.data.features[0].properties.lat);
+    console.log("Longitude", res.data.features[0].properties.lon);
+    console.log("Latitude", res.data.features[0].properties.lat);
+    console.log("Latitude", res.data);
   })
+}
+const getCity =(e)=>{
+  setCity(e.target.value)
+}
+
+const getCountry =(e)=>{
+  setCountry(e.target.value)
+}
+
+const getLocation = (e) =>{
+  e.preventDefault();
+  search();
+}
+
   return (
    
     <div className="App">
-  
+      <form onSubmit={getLocation}>
+        <div className= "form">
+        <input onChange = {getCity} value={city}/>
+        <input onChange = {getCountry} value={country}/>
+        </div>
+        <button>Click me</button>
+      </form>
+
 
    
   

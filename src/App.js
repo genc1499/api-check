541cde25c9f0db68e0d7285dc0167f184a8cc1c4
Joe Gencarelli <jp.genc@gmail.com>
2022-07-14 00:22:29 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  axios({
+    url: 'http://api.positionstack.com/v1/forward',
+    params: {
+      access_key: '57c7fafe4d4cd9ab9aaf1450c00060fa',
+      query: 'Toronto',
+    }
+  }).then((res)=>{
+    console.log(res.data);
+  })
   return (
+   
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  
+
+   
+  
     </div>
   );
 }
